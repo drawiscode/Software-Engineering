517c6b37f65f8829001f176e16e779e9cc73aa3f
drawiscode <1768409568@qq.com>
2025-10-31 15:29:43 +0800

    保存修改：准备同步远程仓库

--- main.cpp
+++ main.cpp
@@ -42,3 +42,19 @@ int main()
     }
     return 0;
 }
+/*
+#ifndef ACCOUNT
+#define ACCOUNT
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void Set_Often_Used_Account();
+void Store_The_Lists();//关闭程序前将新的记录表存入文件中
+void Login_with_Account();//luowenyuz
+
+#endif
+
+*/
